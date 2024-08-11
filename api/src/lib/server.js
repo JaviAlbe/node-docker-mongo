58293f8005969e368349cf121d03c265bab75115
Javier Albeniz <j.albenizlopez@gmail.com>
2024-08-11 22:34:18 +0200

    Initial configuration completed
    - Added and .env file for development and a config.js file to store variables coming from the .env file

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -1,12 +1,13 @@
 import express from "express"
 import * as http from "http";
+import config from "./config.js";
 
 export const startServer = () => {
     const httpServer = express()
-
+    const port = config.port
     try {
-        httpServer.listen(3000, () => {
-            console.log('Server running on port 3000')
+        httpServer.listen(port, () => {
+            console.log(`Server running on port ${port}`)
         })
     } catch (error) {
         throw new Error(error)
